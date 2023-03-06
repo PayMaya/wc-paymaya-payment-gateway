2076bea8eeef1db8304ab97166058861d8ad69d8
Micah Bule <mico@cynder.io>
2023-03-06 17:08:59 +0800

    chore: bump to version 1.1.2

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Maya Business Plugin ===
 Tags: payments, credit card
 Requires at least: 5.0
-Tested up to: 5.9
+Tested up to: 6.1
 Requires PHP: 5.6
-Stable tag: 1.1.1
+Stable tag: 1.1.2
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
--- wc-paymaya-payment-gateway.php
+++ wc-paymaya-payment-gateway.php
@@ -5,11 +5,11 @@
  * Description: Take credit and debit card payments via Maya.
  * Author: PayMaya
  * Author URI: https://www.paymaya.com
- * Version: 1.1.1
+ * Version: 1.1.2
  * Requires at least: 5.3.2
- * Tested up to: 6.0.2
+ * Tested up to: 6.1.1
  * WC requires at least: 3.9.3
- * WC tested up to: 7.2.2
+ * WC tested up to: 7.3.0
  *
  * @category Plugin
  * @package  CynderTech
@@ -53,7 +53,7 @@ function Paymaya_Init_Gateway_class()
     }
 
     define('CYNDER_PAYMAYA_MAIN_FILE', __FILE__);
-    define('CYNDER_PAYMAYA_VERSION', '1.1.1');
+    define('CYNDER_PAYMAYA_VERSION', '1.1.2');
     define('CYNDER_PAYMAYA_BASE_SANDBOX_URL',  'https://pg-sandbox.paymaya.com');
     define('CYNDER_PAYMAYA_BASE_PRODUCTION_URL',  'https://pg.maya.ph');
     define(
